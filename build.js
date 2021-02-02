f31698c2cb7e0250497989617c575f1cae04dcf1
KevinGervais <kevin.gervais@tutanota.com>
2021-02-02 15:58:43 -0500

    fix

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
-var fs = require('fs')
+var ncp = require('ncp').ncp
 async function copyFiles(from, to) {
   console.info(`Copy files from ${from} to ${to}`)
-  await fs.mkdir(to)
   return new Promise((resolve, reject) => {
     ncp(from, to, function (err) {
       if (err) reject(err)
@@ -12,6 +11,6 @@ async function copyFiles(from, to) {
 
 async function build () {
   await copyFiles("./node_modules/src", "./client/src/fontAwesome")
-  copyFiles("./node_modules/icons", "./client/src/icons")
+  await copyFiles("./node_modules/icons", "./client/src/icons")
 }
 build()
