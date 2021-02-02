83fe3e3a68ebf7c8a64f54f234f2e1540638a05d
KevinGervais <kevin.gervais@tutanota.com>
2021-02-02 11:57:53 -0500

    small linting

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+
 import * as brands from "./brands"
 import * as light from "./light"
 import * as regular from "./regular"
@@ -6,30 +7,29 @@ import * as solid from "./solid"
 import { IconType, IconProps, IconFn } from "./model"
 import { IconStyled } from "./IconStyled"
 
-export function Icon<Type extends IconType>(props: IconProps<Type>)  {
-    const {iconType, iconName} = props
-    let Svg: IconFn
-    switch (iconType) {
-        case "brands":
-            Svg = brands[iconName as keyof typeof brands]
-            break;
-        case "light":
-            Svg = light[iconName as keyof typeof light]
-            break;
-        case "solid":
-            Svg = solid[iconName as keyof typeof solid]
-            break;
-        case "regular":
-            Svg = regular[iconName as keyof typeof regular]
-            break;
-        default:
-            Svg = light[iconName as keyof typeof light]
-            break;
-    }
-    return (
-        <IconStyled>
-            <Svg/>
-        </IconStyled>
-    
-    )
+export function Icon<Type extends IconType>(props: IconProps<Type>): JSX.Element {
+  const { iconType, iconName } = props
+  let Svg: IconFn
+  switch (iconType) {
+    case "brands":
+      Svg = brands[iconName as keyof typeof brands]
+      break
+    case "light":
+      Svg = light[iconName as keyof typeof light]
+      break
+    case "solid":
+      Svg = solid[iconName as keyof typeof solid]
+      break
+    case "regular":
+      Svg = regular[iconName as keyof typeof regular]
+      break
+    default:
+      Svg = light[iconName as keyof typeof light]
+      break
+  }
+  return (
+    <IconStyled>
+      <Svg />
+    </IconStyled>
+  )
 }
