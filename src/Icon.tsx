b1445a400b5c3d4b1d869357773b5e11efc5183c
KevinGervais <kevin.gervais@tutanota.com>
2021-02-02 11:50:33 -0500

    first commit

---
+++ src/Icon.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import * as brands from "./brands"
+import * as light from "./light"
+import * as regular from "./regular"
+import * as solid from "./solid"
+import { IconType, IconProps, IconFn } from "./model"
+import { IconStyled } from "./IconStyled"
+
+export function Icon<Type extends IconType>(props: IconProps<Type>)  {
+    const {iconType, iconName} = props
+    let Svg: IconFn
+    switch (iconType) {
+        case "brands":
+            Svg = brands[iconName as keyof typeof brands]
+            break;
+        case "light":
+            Svg = light[iconName as keyof typeof light]
+            break;
+        case "solid":
+            Svg = solid[iconName as keyof typeof solid]
+            break;
+        case "regular":
+            Svg = regular[iconName as keyof typeof regular]
+            break;
+        default:
+            Svg = light[iconName as keyof typeof light]
+            break;
+    }
+    return (
+        <IconStyled>
+            <Svg/>
+        </IconStyled>
+    
+    )
+}
---
+++ src/model.ts
@@ -0,0 +1,21 @@
+import { SVGAttributes } from "react"
+import * as brands from "./brands"
+import * as light from "./light"
+import * as regular from "./regular"
+import * as solid from "./solid"
+
+export type IconType = "brands" | "light" | "regular" | "solid"
+
+export interface IconProps<Type extends IconType> {
+    iconType?: Type
+    iconName: Type extends "brands"
+    ? keyof typeof brands
+    : Type extends "light"
+    ? keyof typeof light
+    : Type extends "regular"
+    ? keyof typeof regular
+    : Type extends "solid"
+    ? keyof typeof solid
+    : keyof typeof light
+}
+export type IconFn = (props: SVGAttributes<{}>) => JSX.Element
