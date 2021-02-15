fd0ff04022ec2e7b660a26d39fc1c47066fd7ccb
KevinGervais <39884567+KevinGervais@users.noreply.github.com>
2021-02-15 10:21:15 -0500

    Update IconStyled.tsx

--- src/IconStyled.tsx
+++ src/IconStyled.tsx
@@ -1,4 +1,11 @@
+import { center, clickable } from "@/styles/mixins"
 import styled from "styled-components"
 
 export const IconStyled = styled.div`
+  ${clickable}
+  ${center}
+  svg {
+    width: 100%;
+    height: 100%;
+  }
 `
